d16e7ea3f3b920ba4c29c2cc55c688a0ed30b338
Tetiana <t.pasichnyk@ukr.net>
2023-03-27 19:53:57 +0300

    upd

--- packages/website/src/http/index.ts
+++ packages/website/src/http/index.ts
@@ -8,8 +8,6 @@ const $api = axios.create({
 });
 
 $api.interceptors.request.use((config) => {
-  console.log(config);
-  console.log(localStorage.getItem("token"));
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
