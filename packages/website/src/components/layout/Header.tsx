463ed62aed60d2ab41b947fa8a2c88fbae1c6e01
Tetiana <t.pasichnyk@ukr.net>
2023-04-03 22:03:40 +0300

    prettier

--- packages/website/src/components/layout/Header.tsx
+++ packages/website/src/components/layout/Header.tsx
@@ -1,123 +1,124 @@
 import "./Header.less";
-import {Button, Col, Row, Typography, Menu, Dropdown, Space} from "antd";
-import {type FC, useContext, useEffect, useState} from "react";
-import {Link, NavLink} from "react-router-dom";
+import { Button, Col, Row, Typography, Menu, Dropdown, Space } from "antd";
+import { type FC, useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import img from "./img/logo.png";
-import type {MenuProps} from "antd";
-import {observer} from "mobx-react-lite";
-import {useBreakpoints} from "../../components/screen";
+import type { MenuProps } from "antd";
+import { observer } from "mobx-react-lite";
+import { useBreakpoints } from "../../components/screen";
 import Login from "./Login";
-import {Cart} from "./Cart";
-import {Context} from "../app";
-import {Navigation} from "./Navigation";
+import { Cart } from "./Cart";
+import { Context } from "../app";
+import { Navigation } from "./Navigation";
 
 const Header: FC = () => {
-    const [open, setOpen] = useState(false);
-    const {store} = useContext(Context);
-    const {isDesktop, isSM} = useBreakpoints();
-    const handleOpenChange = (flag: boolean) => {
-        setOpen(flag);
-    };
-    useEffect(() => {
-        if (localStorage.getItem("token") && !store.isLoading) {
-            store.checkCart();
-        }
-        console.log(store.cart);
-    }, []);
+  const [open, setOpen] = useState(false);
+  const { store } = useContext(Context);
+  const { isDesktop, isSM } = useBreakpoints();
+  const handleOpenChange = (flag: boolean) => {
+    setOpen(flag);
+  };
+  useEffect(() => {
+    if (localStorage.getItem("token") && !store.isLoading) {
+      store.checkCart();
+    }
+    console.log(store.cart);
+  }, []);
 
-    const items: MenuProps["items"] = [
-        {
-            label: <Login/>,
-            key: "0",
-        },
-        {
-            label: (
-                <div>
-                    <NavLink to="/registration">
-                        <Button type="primary" shape="round" size={"large"}>
-                            Registration
-                        </Button>
-                    </NavLink>
-                </div>
-            ),
-            key: "1",
-        },
-    ];
+  const items: MenuProps["items"] = [
+    {
+      label: <Login />,
+      key: "0",
+    },
+    {
+      label: (
+        <div>
+          <NavLink to="/registration">
+            <Button type="primary" shape="round" size={"large"}>
+              Registration
+            </Button>
+          </NavLink>
+        </div>
+      ),
+      key: "1",
+    },
+  ];
 
-    return (
-        <>
-            <Row wrap={false} align={"middle"} justify={"space-between"}>
-                <Col span={isSM ? 3 : 6}>
-                    <Link to={"/"}>
-                        {isDesktop ? <img width={"50%"} src={img}/> : <Navigation/>}
-                    </Link>
-                </Col>
-                <Col
-                    style={{
-                        display: "flex",
-                        justifyContent: isSM ? "start" : "center",
-                        alignItems: "center",
-                    }}
-                    span={isSM ? 10 : 12}>
-                    {isDesktop ? (<Navigation/>
-                    ) : (
-                        <img width={"50%"} src={img}/>
-                    )}
-                </Col>
-                {store.isAuth ? (
-                    <Col style={{justifyContent: "end"}} span={isSM ? 11 : 6}>
-                        <Row gutter={8} justify={"space-between"}>
-                            <Cart/>
-                            <Col>
-                                <Typography.Paragraph style={{margin: 0}}>
-                                   ({store.cart ? store.cart.reduce(function(sum, current) {
-                                        return sum + current.quantity;
-                                    }, 0) : 0})
-                                    {/*({store.cart.length})*/}
-                                </Typography.Paragraph>
-                            </Col>
-                            <Col>
-                                <Button
-                                    onClick={async () => {
-                                        await store.logout();
-                                    }}
-                                    type="primary"
-                                    htmlType="submit"
-                                >
-                                    Log out
-                                </Button>
-                            </Col>
-                        </Row>
-                        {/*</div>*/}
-                    </Col>
-                ) : (
-                    <Col
-                        style={{
-                            display: "flex",
-                            justifyContent: "end",
-                        }}
-                        span={isSM ? 11 : 6}>
-                        <Dropdown
-                            menu={{items}}
-                            open={open}
-                            autoFocus={false}
-                            onOpenChange={handleOpenChange}
-                            trigger={["click"]}
-                        >
-                            <a
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                }}
-                            >
-                                <Space>Log in</Space>
-                            </a>
-                        </Dropdown>
-                    </Col>
-                )}
-
+  return (
+    <>
+      <Row wrap={false} align={"middle"} justify={"space-between"}>
+        <Col span={isSM ? 3 : 6}>
+          <Link to={"/"}>
+            {isDesktop ? <img width={"50%"} src={img} /> : <Navigation />}
+          </Link>
+        </Col>
+        <Col
+          style={{
+            display: "flex",
+            justifyContent: isSM ? "start" : "center",
+            alignItems: "center",
+          }}
+          span={isSM ? 10 : 12}
+        >
+          {isDesktop ? <Navigation /> : <img width={"50%"} src={img} />}
+        </Col>
+        {store.isAuth ? (
+          <Col style={{ justifyContent: "end" }} span={isSM ? 11 : 6}>
+            <Row gutter={8} justify={"space-between"}>
+              <Cart />
+              <Col>
+                <Typography.Paragraph style={{ margin: 0 }}>
+                  (
+                  {store.cart
+                    ? store.cart.reduce(function (sum, current) {
+                        return sum + current.quantity;
+                      }, 0)
+                    : 0}
+                  ){/*({store.cart.length})*/}
+                </Typography.Paragraph>
+              </Col>
+              <Col>
+                <Button
+                  onClick={async () => {
+                    await store.logout();
+                  }}
+                  type="primary"
+                  htmlType="submit"
+                >
+                  Log out
+                </Button>
+              </Col>
             </Row>
-        </>
-    );
+            {/*</div>*/}
+          </Col>
+        ) : (
+          <Col
+            style={{
+              display: "flex",
+              justifyContent: "end",
+            }}
+            span={isSM ? 11 : 6}
+          >
+            <Dropdown
+              menu={{ items }}
+              open={open}
+              autoFocus={false}
+              onOpenChange={handleOpenChange}
+              trigger={["click"]}
+            >
+              <a
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                <Space>Log in</Space>
+              </a>
+            </Dropdown>
+          </Col>
+        )}
+      </Row>
+    </>
+  );
 };
 
 export default observer(Header);
--- packages/website/src/pages/home/sections/Shop.tsx
+++ packages/website/src/pages/home/sections/Shop.tsx
@@ -1,92 +1,93 @@
 import "./Shop.less";
 
-import {List, Typography, Card} from "antd";
-import {Link} from "react-router-dom";
-import {type FC, useEffect, useState, CSSProperties} from "react";
+import { List, Typography, Card } from "antd";
+import { Link } from "react-router-dom";
+import { type FC, useEffect, useState, CSSProperties } from "react";
 import SectionContainer from "../../../components/section/SectionContainer";
-import {ButtonCart} from "../../../components/layout/ButtonCart";
-import {useBreakpoints} from "../../../components/screen";
+import { ButtonCart } from "../../../components/layout/ButtonCart";
+import { useBreakpoints } from "../../../components/screen";
 import ContentService from "../../../services/ContentServices";
 
 const Shop: FC = () => {
-    const [cards, setCards] = useState<Product[]>([]);
-    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [cards, setCards] = useState<Product[]>([]);
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-    const { isDesktop,  isSM } = useBreakpoints();
-    useEffect(() => {
-        async function getProducts() {
-            try {
-                const response = await ContentService.fetchAllProducts();
-                setCards(response.data);
-            } catch (e) {
-                console.log(e);
-            }
-        }
+  const { isDesktop, isSM } = useBreakpoints();
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await ContentService.fetchAllProducts();
+        setCards(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
 
-        getProducts();
-    }, []);
+    getProducts();
+  }, []);
 
-    return (
-        <SectionContainer>
-            <List
-                style={{maxWidth: "100% "}}
-                itemLayout="vertical"
-                size="large"
-                dataSource={cards || []}
-                grid={{
-                    gutter: 20,
-                    xs: 1,
-                    md: 2,
-                    lg: 2,
-                    xl: 3,
-                    xxl: 3,
-                }}
-                pagination={{pageSize: 6, hideOnSinglePage: true}}
-                renderItem={(item, index) => {
+  return (
+    <SectionContainer>
+      <List
+        style={{ maxWidth: "100% " }}
+        itemLayout="vertical"
+        size="large"
+        dataSource={cards || []}
+        grid={{
+          gutter: 20,
+          xs: 1,
+          md: 2,
+          lg: 2,
+          xl: 3,
+          xxl: 3,
+        }}
+        pagination={{ pageSize: 6, hideOnSinglePage: true }}
+        renderItem={(item, index) => {
+          const isActive = activeIndex === index;
+          const className = `product-card ${isActive ? "active" : ""}`;
 
-                    const isActive = activeIndex === index;
-                    const className = `product-card ${isActive ? "active" : ""}`;
+          const handleClick = (index: number) => {
+            setActiveIndex(index);
+          };
+          const linkStyles: CSSProperties = !isDesktop
+            ? index === activeIndex
+              ? { pointerEvents: "auto" }
+              : { pointerEvents: "none" }
+            : { pointerEvents: "auto" };
 
-                    const handleClick = (index: number) => {
-                        setActiveIndex(index);
-                    };
-                    const linkStyles: CSSProperties = !isDesktop
-                        ? index === activeIndex
-                            ? {pointerEvents: "auto"}
-                            : {pointerEvents: "none"}
-                        : {pointerEvents: "auto"};
-
-                    return (<List.Item
-                        style={index >= 1 ? {animationDelay: `0.${index}s`} : {}}
-                        key={item?._id}
-                        onClick={() => handleClick(index)}
-                        className={className}
-                    >
-                        <Card hoverable style={{background: "transparent"}}>
-                            {" "}
-                            <Link  style={linkStyles} to={`/product/${item?._id}`}>
-                                {item.photo && (
-                                    <img
-                                        style={{width: "100%"}}
-                                        src={item.photo}
-                                        className="img"
-                                    />
-                                )}
-                                <Typography.Title level={3}>{item?.title}</Typography.Title>
-                                <Typography.Paragraph type={"secondary"}>
-                                    {item?.description}
-                                </Typography.Paragraph>
-                                <Typography.Paragraph>
-                                    {item?.price?.toString()} $
-                                </Typography.Paragraph>
-                            </Link>
-                            <ButtonCart productId={item?._id}/>
-                        </Card>
-                    </List.Item>)
-                }}
-            />
-        </SectionContainer>
-    );
+          return (
+            <List.Item
+              style={index >= 1 ? { animationDelay: `0.${index}s` } : {}}
+              key={item?._id}
+              onClick={() => handleClick(index)}
+              className={className}
+            >
+              <Card hoverable style={{ background: "transparent" }}>
+                {" "}
+                <Link style={linkStyles} to={`/product/${item?._id}`}>
+                  {item.photo && (
+                    <img
+                      style={{ width: "100%" }}
+                      src={item.photo}
+                      className="img"
+                    />
+                  )}
+                  <Typography.Title level={3}>{item?.title}</Typography.Title>
+                  <Typography.Paragraph type={"secondary"}>
+                    {item?.description}
+                  </Typography.Paragraph>
+                  <Typography.Paragraph>
+                    {item?.price?.toString()} $
+                  </Typography.Paragraph>
+                </Link>
+                <ButtonCart productId={item?._id} />
+              </Card>
+            </List.Item>
+          );
+        }}
+      />
+    </SectionContainer>
+  );
 };
 
-export {Shop};
+export { Shop };
