d088b4d3d4a2a6fca243b5138fdf7b82a49d53d1
Tetiana <t.pasichnyk@ukr.net>
2023-04-10 12:32:27 +0300

    upd: product page

--- packages/website/src/pages/product/index.tsx
+++ packages/website/src/pages/product/index.tsx
@@ -6,12 +6,14 @@ import SectionContainer from "../../components/section/SectionContainer";
 import {useParams} from "react-router-dom";
 import ContentService from "../../services/ContentServices";
 import {ButtonCart} from "../../components/layout/ButtonCart";
+import {useBreakpoints} from "../../components/screen";
 
 // import { We } from "./sections/index";
 
 const Product: FC = () => {
     const {id} = useParams();
     const [card, setCard] = useState<Partial<Product>>({});
+    const { isDesktop, isSM } = useBreakpoints();
 
     useEffect(() => {
         async function getProducts(x: string) {
@@ -29,10 +31,10 @@ const Product: FC = () => {
     return (
             <SectionContainer>
                 <Row style={{alignItems: 'center'}} justify={'space-between'}>
-                    <Col span={10}>
-                        <Image src={card.photo}/>
+                    <Col span={isDesktop ? 10 : 24}>
+                        {isDesktop ? <Image src={card.photo}/> : <img width={'100%'} src={card.photo} alt={'product photo'}/>}
                     </Col>
-                    <Col span={14}>
+                    <Col span={isDesktop ? 14 : 24}>
                         <Typography.Title level={3}>
                             {card.title}
                         </Typography.Title>
